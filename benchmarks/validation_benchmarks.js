82a95591376054d352673bfb3bd44c66dbb20d2c
Christian Kvalheim <christkv@gmail.com>
2015-07-22 15:46:28 +0200

    Array code generation working correctly

--- benchmarks/validation_benchmarks.js
+++ benchmarks/validation_benchmarks.js
@@ -7,18 +7,18 @@ var Joi = require('joi');
 // Create a benchmark suite
 var suite = new Benchmark.Suite;
 
-// Create a schema instance
-var schema = new Builder();
-// Set up schema rule for the number field
-schema.rule('.number')
-  .type(Number)
-  .exists(true)
-  .validate({
-    $gt: 100, $lt: 1000
-  });
-
-// Complile the expression
-var expression = new Compiler().compile(schema);
+// // Create a schema instance
+// var schema = new Builder();
+// // Set up schema rule for the number field
+// schema.rule('.number')
+//   .type(Number)
+//   .exists(true)
+//   .validate({
+//     $gt: 100, $lt: 1000
+//   });
+//
+// // Complile the expression
+// var expression = new Compiler().compile(schema);
 
 // var schema = Joi.obj ect().keys({
 //     username: Joi.string().alphanum().min(3).max(30).required(),
@@ -32,10 +32,10 @@ var expression = new Compiler().compile(schema);
 //   console.dir(err)
 // });
 
-// Create Joi expression
-var joiSchema = Joi.object().keys({
-  number: Joi.number().integer().min(100).max(1000)
-}).requiredKeys('number');
+// // Create Joi expression
+// var joiSchema = Joi.object().keys({
+//   number: Joi.number().integer().min(100).max(1000)
+// }).requiredKeys('number');
 
 // Joi.validate({number:15}, joiSchema, function(err, value) {
 //   console.dir(err)
@@ -58,17 +58,17 @@ var joiSchema = Joi.object().keys({
 
 //
 
-suite.add('Validator compile', function() {
-  var object = {
-    number: 150
-  };
-  expression.validate(object);
-})
-.add('Joi validator', function() {
-  Joi.validate({number:105}, joiSchema, {abortEarly:false}, function(err, value) {})
-})
+// suite.add('Validator compile', function() {
+//   var object = {
+//     number: 150
+//   };
+//   expression.validate(object);
+// })
+// .add('Joi validator', function() {
+//   Joi.validate({number:105}, joiSchema, {abortEarly:false}, function(err, value) {})
+// })
 
-.add('Handcoded validation', function() {
+suite.add('Handcoded validation', function() {
   var errors = [];
   var rules = [];
   var object = {
@@ -94,6 +94,31 @@ suite.add('Validator compile', function() {
   }
 
 })
+
+.add('Handcoded validation func', function() {
+  var errors = [];
+  var rules = [];
+  var object = {
+    number: 150
+  };
+
+  // functions.func1(object);
+  //
+  // functions.func2(object);
+  //
+  // functions.func3(object);
+
+  // functions[0](object);
+  //
+  // functions[1](object);
+  //
+  // functions[2](object);
+
+  func1(object);
+  func2(object);
+  func3(object);
+})
+
 .on('cycle', function(event) {
   console.log(String(event.target));
 })
@@ -102,3 +127,79 @@ suite.add('Validator compile', function() {
 })
 // run async
 .run({ 'async': true });
+var functions = [
+  function(object) {
+    if(object.number == null && true) {
+      errors.push(new ValidationError('field .number does not exist', rules[0]));
+    } else if(object.number == null) {
+      throw new ValidationError('field .number does not exist', rules[0]);
+    }
+  },
+
+  function(object) {
+    if(object.number != null && !(object.number.constructor.name == "Number") && true) {
+      errors.push(new ValidationError('field .number is not of expected type function Number() { [native code] }', rules[0]));
+    } else if(object.number != null && !(object.number.constructor.name == "Number")) {
+      throw new ValidationError('field .number is not of expected type function Number() { [native code] }', rules[0]);
+    }
+  },
+
+  function(object) {
+    if(!(object.number > 100 && object.number < 1000) && true) {
+      errors.push(new ValidationError('field .number fails validation', rules[0]));
+    } else if(!(object.number > 100 && object.number < 1000)) {
+      throw new ValidationError('field .number fails validation', rules[0]);
+    }
+  }
+]
+
+var func1 = function(object) {
+  if(object.number == null && true) {
+    errors.push(new ValidationError('field .number does not exist', rules[0]));
+  } else if(object.number == null) {
+    throw new ValidationError('field .number does not exist', rules[0]);
+  }
+}
+
+var func2 = function(object) {
+  if(object.number != null && !(object.number.constructor.name == "Number") && true) {
+    errors.push(new ValidationError('field .number is not of expected type function Number() { [native code] }', rules[0]));
+  } else if(object.number != null && !(object.number.constructor.name == "Number")) {
+    throw new ValidationError('field .number is not of expected type function Number() { [native code] }', rules[0]);
+  }
+}
+
+var func3 = function(object) {
+  if(!(object.number > 100 && object.number < 1000) && true) {
+    errors.push(new ValidationError('field .number fails validation', rules[0]));
+  } else if(!(object.number > 100 && object.number < 1000)) {
+    throw new ValidationError('field .number fails validation', rules[0]);
+  }
+}
+
+
+// var functions = {
+//   func1: function(object) {
+//     if(object.number == null && true) {
+//       errors.push(new ValidationError('field .number does not exist', rules[0]));
+//     } else if(object.number == null) {
+//       throw new ValidationError('field .number does not exist', rules[0]);
+//     }
+//   },
+//
+//   func2: function(object) {
+//     if(object.number != null && !(object.number.constructor.name == "Number") && true) {
+//       errors.push(new ValidationError('field .number is not of expected type function Number() { [native code] }', rules[0]));
+//     } else if(object.number != null && !(object.number.constructor.name == "Number")) {
+//       throw new ValidationError('field .number is not of expected type function Number() { [native code] }', rules[0]);
+//     }
+//   },
+//
+//   func3: function(object) {
+//     if(!(object.number > 100 && object.number < 1000) && true) {
+//       errors.push(new ValidationError('field .number fails validation', rules[0]));
+//     } else if(!(object.number > 100 && object.number < 1000)) {
+//       throw new ValidationError('field .number fails validation', rules[0]);
+//     }
+//   }
+// }
--- test/ast_tests.js
+++ test/ast_tests.js
@@ -9,22 +9,9 @@ var assert = require("assert"),
 describe('Number', function() {
   describe('validation', function() {
     it('should error due to missing number field', function() {
-      // Another ast for array documents
-      var arrayDocument = new AST({
-        'user': {
-          type: String, exits:true, customAsync: new Custom({}, function(string, field, context, callback) {
-            if(string.length == 0) {
-              return callback(f('field %s must be longer than 0 characters', field));
-            }
-
-            callback();
-          })
-        }
-      });
-
       var embeddedDocument = new AST({
         'field': {
-          type: String, custom: new Custom({regexp: /dog/}, function(string, field, context) {
+          type: String, exists:true, custom: new Custom({regexp: /dog/}, function(string, field, context) {
             if(string.match(context.regexp) == null) {
               return new Error(f('field %s does not match %', field, context.regexp));
             }
@@ -32,11 +19,29 @@ describe('Number', function() {
         }
       });
 
+      // Another ast for array documents
+      var arrayDocument = new AST({
+        'user': {
+          type: String, exists:true, customAsync: new Custom({}, function(string, field, context, callback) {
+            if(string.length == 0) {
+              return callback(f('field %s must be longer than 0 characters', field));
+            }
+
+            callback();
+          })
+        },
+        'users': {
+          type: Array, exists:true, validations: {
+            $gte: 1, $lte: 16
+          }, of: embeddedDocument
+        }
+      });
+
       // Top level document
       var topLevelDocument = new AST({
         'number' : {
           type: Number, exists:true, validation: {
-            $gt: 100, $lt: 1000
+            $gt: 100, $lt: 1000, $in: [100, 200]
           }
         },
         'string' : {
@@ -55,13 +60,98 @@ describe('Number', function() {
       });
 
       var compiler = new Compiler({
-        logger: {
-          info: function(string) {
-            console.log(string)
-          }
-        }
+        // logger: {
+        //   info: function(string) {
+        //     console.log(string)
+        //   }
+        // }
       });
+
+      // Compile the AST
       var func = compiler.compile(topLevelDocument);
+      // Execute validations
+      var results = func.validate({});
+      assert.equal(3, results.length);
+      assert.equal('number', results[0].field)
+      assert.equal('object', results[0].parent)
+      
+      assert.equal('string', results[1].field)
+      assert.equal('object', results[1].parent)
+      
+      assert.equal('array', results[2].field)
+      assert.equal('object', results[2].parent)
+
+      var results = func.validate({number:1});
+      assert.equal(2, results.length);
+      assert.equal('string', results[0].field)
+      assert.equal('object', results[0].parent)
+      
+      assert.equal('array', results[1].field)
+      assert.equal('object', results[1].parent)
+
+      var results = func.validate({number:1, string: 'hello'});
+      assert.equal(1, results.length);
+      assert.equal('array', results[0].field)
+      assert.equal('object', results[0].parent)
+
+      var results = func.validate({number:1, string: 'hello', array: []});
+      assert.equal(0, results.length);
+
+      var results = func.validate({number:1, string: 'hello', array: [{}]});
+      assert.equal(2, results.length);
+      assert.equal('user', results[0].field)
+      assert.equal('object.array[0]', results[0].parent)
+      assert.equal('users', results[1].field)
+      assert.equal('object.array[0]', results[1].parent)
+
+      var results = func.validate({number:1, string: 'hello', array: [{}, {}]});
+      assert.equal(4, results.length);
+      assert.equal('user', results[0].field)
+      assert.equal('object.array[0]', results[0].parent)
+      assert.equal('users', results[1].field)
+      assert.equal('object.array[0]', results[1].parent)
+      assert.equal('user', results[2].field)
+      assert.equal('object.array[1]', results[2].parent)
+      assert.equal('users', results[3].field)
+      assert.equal('object.array[1]', results[3].parent)
+
+      var results = func.validate({number:1, string: 'hello', array: [{user:'user'}]});
+      assert.equal(1, results.length);
+      assert.equal('users', results[0].field)
+      assert.equal('object.array[0]', results[0].parent)
+
+      var results = func.validate({number:1, string: 'hello', array: [{user:'user', users:[]}]});
+      assert.equal(0, results.length);
+
+      var results = func.validate({number:1, string: 'hello', array: [{user:'user', users:[{}]}]});
+      assert.equal(1, results.length);
+      assert.equal('field', results[0].field)
+      assert.equal('object.array[0].users[0]', results[0].parent)
+
+      var results = func.validate({number:1, string: 'hello', array: [{user:'user', users:[{}, {}]}]});
+      assert.equal(2, results.length);
+      assert.equal('field', results[0].field)
+      assert.equal('object.array[0].users[0]', results[0].parent)
+      assert.equal('field', results[1].field)
+      assert.equal('object.array[0].users[1]', results[1].parent)
+
+      var results = func.validate({number:1, string: 'hello', array: [{user:'user', users:[]}, {user:'user', users:[{}]}]});
+      assert.equal(1, results.length);
+      assert.equal('field', results[0].field)
+      assert.equal('object.array[1].users[0]', results[0].parent)
+      // assert.equal('field', results[1].field)
+      // assert.equal('object.array[0].users[1]', results[1].parent)
+
+      // assert.equal('array', results[0].field)
+      // assert.equal('object', results[0].parent)
+
+      // // // console.dir(func)
+      // var results = func.validate({number: 1, string: 'hello', array:[{user:2, users: [{t:1}]}]})
+      //
+      // // Get all the results
+      console.log("------------------------------------------------------------ final results")
+      // console.log(JSON.stringify(results, null, 2))
+      console.dir(results)
     });
   });
 });
