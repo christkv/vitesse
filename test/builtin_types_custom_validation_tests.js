959e10152ef700acd0fc862f6006e85e94071dfa
Christian Kvalheim <christkv@gmail.com>
2015-08-13 16:14:04 +0200

    Added custom validation support for string inbuilt type

---
+++ test/builtin_types_custom_validation_tests.js
@@ -0,0 +1,47 @@
+var assert = require("assert"),
+  co = require('co'),
+  f = require('util').format,
+  ArrayType = require('../lib/ast').ArrayType,
+  NestedArrayType = require('../lib/ast').NestedArrayType,
+  CustomType = require('../lib/ast').CustomType,
+  StringType = require('../lib/ast').StringType,
+  NumberType = require('../lib/ast').NumberType,
+  DocumentType = require('../lib/ast').DocumentType,
+  Custom = require('../lib/custom'),
+  Builder = require('../lib/builder'),
+  Compiler = require('../lib/compiler');
+
+describe('Custom', function() {
+  describe('builtin custom validations', function() {
+    it('simple string type validation extended with custom validation functions', function() {
+      var schema = new DocumentType({
+        'field': new StringType({
+          custom: [new CustomType({
+            context: {valid: 'dog'}, 
+            func: function(object, context) {
+              if(object != context.valid) {
+                return new Error('field did not contain dog');
+              }
+            }
+          })]
+        }, {})
+      });
+
+      var compiler = new Compiler({});
+      // Compile the AST
+      var func = compiler.compile(schema, {debug:true});
+
+      // Validate {field: ''}
+      var results = func.validate({field: ''});
+      assert.equal(1, results.length);
+      assert.equal('field did not contain dog', results[0].message);
+      assert.equal('object.field', results[0].path);
+      assert.equal('', results[0].value);
+      assert.ok(results[0].rule instanceof DocumentType);
+
+      // Validate {field: 'dog'}
+      var results = func.validate({field: 'dog'});
+      assert.equal(0, results.length);
+    });
+  });
+});
