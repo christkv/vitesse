5d69f85df4ceb1e7b02afe44e158a41982ee1c3c
Christian Kvalheim <christkv@gmail.com>
2015-08-13 13:17:34 +0200

    Added more test harnesses

---
+++ test/numeric_validation_tests.js
@@ -0,0 +1,18 @@
+var assert = require("assert"),
+  co = require('co'),
+  f = require('util').format,
+  ArrayType = require('../lib/ast').ArrayType,
+  NestedArrayType = require('../lib/ast').NestedArrayType,
+  StringType = require('../lib/ast').StringType,
+  NumberType = require('../lib/ast').NumberType,
+  DocumentType = require('../lib/ast').DocumentType,
+  Custom = require('../lib/custom'),
+  Builder = require('../lib/builder'),
+  Compiler = require('../lib/compiler');
+
+describe('Number', function() {
+  describe('validation', function() {
+    it('should perform triple nested array validations [][][]', function() {
+    });
+  });
+});
