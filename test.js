3da7ee276825ddbfe40aea4de85b09b358b17fda
Christian Kvalheim <christkv@gmail.com>
2015-09-04 23:04:42 +0200

    refactorings to simplify logic

--- test.js
+++ test.js
@@ -11,6 +11,14 @@ var ValidationError = function(message, path, rule, value) {
 
 var rules = [];
 
+var ValidationError = function(message, path, rule, value, errors) {
+  this.message = message;
+  this.path = path;
+  this.rule = rule;
+  this.value = value;
+  this.errors = errors;
+}
+
 var validate = function(object, context) {
   var context = context == null ? {} : context;
   var errors = [];
@@ -58,15 +66,67 @@ var validate = function(object, context) {
     }
     return true;
   }
-  var object_validation1 = function(path, object, context) {
-    var errorsLength = errors.length;
-    if ((object == null || typeof object != 'object' || Array.isArray(object)) && true && context.failOnFirst) {
-      throw new ValidationError('field is not an object', path, rules[0], object);
-    } else if ((object == null || typeof object != 'object' || Array.isArray(object)) && true) {
-      errors.push(new ValidationError('field is not an object', path, rules[0], object));
+  var object_validation5 = function(path, object, context) {
+    var currentErrors = errors;
+    errors = [];
+    if ((object == null || typeof object != 'object' || Array.isArray(object)) && context.failOnFirst) {
+      throw new ValidationError('field is not an object', path, rules[4], object);
+    } else if( (object == null || typeof object != 'object' || Array.isArray(object)) ) {
+      errors.push(new ValidationError('field is not an object', path, rules[4], object));
     }
-    if ((object == null || typeof object != 'object' || Array.isArray(object)) && errorsLength < errors.length) {
+    if (!true && errors.length > 0) {
+      errors = currentErrors;
       return;
+    } else if (errors.length > 0) {
+      errors = currentErrors.concat(errors);
+      return;
+    } else {
+      errors = currentErrors;
+    }
+    // Not possible to perform any validations on the object as it does not exist
+    if (object === undefined || object == null) return;
+    // Prohibits fields override
+    // Requires fields override
+    // Validations
+    // Field name pattern validation
+    var fieldNames = {};
+    var keys = Object.keys(object);
+    var properties = keys.slice(0);
+    // The sets
+    var validSet = {};
+    // Go over all the keys
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      if (fieldNames[key]) {
+        // Set the valid key
+        validSet[key] = {};
+        // Remove the property
+        properties.splice(properties.indexOf(key), 1);
+      }
+      // Pattern validations
+    }
+    // Additional properties object
+
+    // Additional properties false
+    // Custom validations
+    // Perform validations on object fields
+  }
+  var object_validation4 = function(path, object, context) {
+    var currentErrors = errors;
+    errors = [];
+    if ((object == null || typeof object != 'object' || Array.isArray(object)) && context.failOnFirst) {
+      throw new ValidationError('field is not an object', path, rules[3], object);
+    } else if( (object == null || typeof object != 'object' || Array.isArray(object)) ) {
+      errors.push(new ValidationError('field is not an object', path, rules[3], object));
+    }
+    if (!true && errors.length > 0) {
+      errors = currentErrors;
+      return;
+    } else if (errors.length > 0) {
+      errors = currentErrors.concat(errors);
+      return;
+    } else {
+      errors = currentErrors;
     }
     // Not possible to perform any validations on the object as it does not exist
     if (object === undefined || object == null) return;
@@ -75,8 +135,7 @@ var validate = function(object, context) {
     // Validations
     // Field name pattern validation
     var fieldNames = {
-      "foo": {},
-      "bar": {}
+      "foo": {}
     };
     var keys = Object.keys(object);
     var properties = keys.slice(0);
@@ -95,34 +154,163 @@ var validate = function(object, context) {
     }
     // Additional properties object
 
-    console.log("------------------------------------------ 1")
     // Additional properties false
     // Custom validations
     // Perform validations on object fields
-    var _object = object.foo;
-    var _path = path + '.foo';
-    if (_object === undefined) return;
-    var errorLength = errors.length;
-    if ((!(typeof _object == 'number') || (typeof _object == 'number' && _object % 1 === 0)) && true && context.failOnFirst) {
-      throw new ValidationError('field is not a number', _path, rules[1], _object);
-    } else if ((!(typeof _object == 'number') || (typeof _object == 'number' && _object % 1 === 0)) && false) {
-      errors.push(new ValidationError('field is not a number', _path, rules[1], _object));
+    object_validation5(path + '.foo', object.foo, context);
+  }
+  var object_validation3 = function(path, object, context) {
+    var currentErrors = errors;
+    errors = [];
+    if ((object == null || typeof object != 'object' || Array.isArray(object)) && context.failOnFirst) {
+      throw new ValidationError('field is not an object', path, rules[2], object);
+    } else if( (object == null || typeof object != 'object' || Array.isArray(object)) ) {
+      errors.push(new ValidationError('field is not an object', path, rules[2], object));
     }
-
-    console.log("------------------------------------------")
-    var _object = object.bar;
-    var _path = path + '.bar';
-    if (_object === undefined) return;
-    var errorLength = errors.length;
-    if ((!(typeof _object == 'number') || (typeof _object == 'number' && _object % 1 === 0)) && true && context.failOnFirst) {
-      throw new ValidationError('field is not a number', _path, rules[2], _object);
-    } else if ((!(typeof _object == 'number') || (typeof _object == 'number' && _object % 1 === 0)) && false) {
-      errors.push(new ValidationError('field is not a number', _path, rules[2], _object));
+    if (!true && errors.length > 0) {
+      errors = currentErrors;
+      return;
+    } else if (errors.length > 0) {
+      errors = currentErrors.concat(errors);
+      return;
+    } else {
+      errors = currentErrors;
     }
+    // Not possible to perform any validations on the object as it does not exist
+    if (object === undefined || object == null) return;
+    // Prohibits fields override
+    // Requires fields override
+    // Validations
+    // Field name pattern validation
+    var fieldNames = {
+      "foo": {}
+    };
+    var keys = Object.keys(object);
+    var properties = keys.slice(0);
+    // The sets
+    var validSet = {};
+    // Go over all the keys
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      if (fieldNames[key]) {
+        // Set the valid key
+        validSet[key] = {};
+        // Remove the property
+        properties.splice(properties.indexOf(key), 1);
+      }
+      // Pattern validations
+    }
+    // Additional properties object
 
+    // Additional properties false
+    // Custom validations
+    // Perform validations on object fields
+    object_validation4(path + '.foo', object.foo, context);
+  }
+  var object_validation2 = function(path, object, context) {
+    var currentErrors = errors;
+    errors = [];
+    if ((object == null || typeof object != 'object' || Array.isArray(object)) && context.failOnFirst) {
+      throw new ValidationError('field is not an object', path, rules[1], object);
+    } else if( (object == null || typeof object != 'object' || Array.isArray(object)) ) {
+      errors.push(new ValidationError('field is not an object', path, rules[1], object));
+    }
+    if (!true && errors.length > 0) {
+      errors = currentErrors;
+      return;
+    } else if (errors.length > 0) {
+      errors = currentErrors.concat(errors);
+      return;
+    } else {
+      errors = currentErrors;
+    }
+    // Not possible to perform any validations on the object as it does not exist
+    if (object === undefined || object == null) return;
+    // Prohibits fields override
+    // Requires fields override
+    // Validations
+    // Field name pattern validation
+    var fieldNames = {
+      "foo": {}
+    };
+    var keys = Object.keys(object);
+    var properties = keys.slice(0);
+    // The sets
+    var validSet = {};
+    // Go over all the keys
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      if (fieldNames[key]) {
+        // Set the valid key
+        validSet[key] = {};
+        // Remove the property
+        properties.splice(properties.indexOf(key), 1);
+      }
+      // Pattern validations
+    }
+    // Additional properties object
+
+    // Additional properties false
+    // Custom validations
+    // Perform validations on object fields
+    object_validation3(path + '.foo', object.foo, context);
+  }
+  var object_validation1 = function(path, object, context) {
+    var currentErrors = errors;
+    errors = [];
+    if ((object == null || typeof object != 'object' || Array.isArray(object)) && context.failOnFirst) {
+      throw new ValidationError('field is not an object', path, rules[0], object);
+    } else if( (object == null || typeof object != 'object' || Array.isArray(object)) ) {
+      errors.push(new ValidationError('field is not an object', path, rules[0], object));
+    }
+    if (!false && errors.length > 0) {
+      errors = currentErrors;
+      return;
+    } else if (errors.length > 0) {
+      errors = currentErrors.concat(errors);
+      return;
+    } else {
+      errors = currentErrors;
+    }
+    // Not possible to perform any validations on the object as it does not exist
+    if (object === undefined || object == null) return;
+    // Prohibits fields override
+    // Requires fields override
+    // Validations
+    // Field name pattern validation
+    var fieldNames = {
+      "foo": {}
+    };
+    var keys = Object.keys(object);
+    var properties = keys.slice(0);
+    // The sets
+    var validSet = {};
+    // Go over all the keys
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      if (fieldNames[key]) {
+        // Set the valid key
+        validSet[key] = {};
+        // Remove the property
+        properties.splice(properties.indexOf(key), 1);
+      }
+      // Pattern validations
+    }
+    // Additional properties object
+
+    // Additional properties false
+    if (properties.length > 0 && context.failOnFirst) {
+      throw new ValidationError('illegal fields on object', path, rules[0], object);
+    } else if (properties.length > 0) {
+      errors.push(new ValidationError('illegal fields on object', path, rules[0], object));
+    }
+    // Custom validations
+    // Perform validations on object fields
+    object_validation2(path + '.foo', object.foo, context);
   }
 
   object_validation1('object', object, context);
   return errors;
 };
-console.dir(validate({bar:true}))
+
+console.dir(validate({"foo": false}))
